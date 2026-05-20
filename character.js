5e140a3a7845268b022b16444bf7c57888831279
Jiang <jiyu24ln@student.ju.se>
2026-05-20 20:38:05 +0200

    make sure the jump speed of the player

--- character.js
+++ character.js
@@ -14,3 +14,20 @@ export default class Character {
     this.vx = 0;
     this.vy = 0;
   }
+
+  move() {
+    this.vy += 0.4;
+    this.x += this.vx;
+    this.y += this.vy;
+    this.x = constrain(this.x, 0, width - this.w);
+  }
+
+  jump() {
+    this.vy = -12;
+  }
+
+  draw() {
+    fill(100, 200, 80);
+    noStroke();
+    ellipse(this.x + this.w / 2, this.y + this.h / 2, this.w, this.h);
+  }
